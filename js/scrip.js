ed75d87f5d98ae4190e69f5e835122947caf4228
JUMAJOMI <jumajuma0902@gmail.com>
2026-09-16 13:25:06 -0500

    yes

--- js/scrip.js
+++ js/scrip.js
@@ -192,3 +192,13 @@ ocultarBadgeNetlify();
 setTimeout(ocultarBadgeNetlify, 1000);
 setTimeout(ocultarBadgeNetlify, 3000);
 setInterval(ocultarBadgeNetlify, 5000);
+
+import { ElevenLabsClient } from "@elevenlabs/elevenlabs-js";
+
+const elevenlabs = new ElevenLabsClient();
+const audio = await elevenlabs
+  .textToSpeech.convert("NOpBlnGInO9m6vDvFkFC", {
+    text: "In the ancient land of Eldoria, where skies shimmered and forests, whispered secrets to the wind, lived a dragon named Zephyros. [sarcastically] Not the “burn it all down” kind... [giggles] but he was gentle, wise, with eyes like old stars. [whispers] Even the birds fell silent when he passed.",
+    modelId: "eleven_v3",
+    languageCode: "en",
+  });
